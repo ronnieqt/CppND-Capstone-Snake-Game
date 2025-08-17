7e34db8c440e5c3772d62df716538cd18ff9bba0
ronnieqt <xi.ronnie.chen@outlook.com>
2025-08-17 08:32:14 +0100

    code formatting

--- src/controller.cpp
+++ src/controller.cpp
@@ -3,18 +3,23 @@
 #include "SDL.h"
 #include "snake.h"
 
-void Controller::ChangeDirection(Snake &snake, Snake::Direction input,
-                                 Snake::Direction opposite) const {
-  if (snake.direction != opposite || snake.size == 1) snake.direction = input;
+void Controller::ChangeDirection(Snake& snake, Snake::Direction input,
+                                 Snake::Direction opposite) const
+{
+  if (snake.direction != opposite || snake.size == 1) {
+    snake.direction = input;
+  }
   return;
 }
 
-void Controller::HandleInput(bool &running, Snake &snake) const {
+void Controller::HandleInput(bool &running, Snake &snake) const
+{
   SDL_Event e;
   while (SDL_PollEvent(&e)) {
     if (e.type == SDL_QUIT) {
       running = false;
-    } else if (e.type == SDL_KEYDOWN) {
+    }
+    else if (e.type == SDL_KEYDOWN) {
       switch (e.key.keysym.sym) {
         case SDLK_UP:
           ChangeDirection(snake, Snake::Direction::kUp,
--- src/controller.h
+++ src/controller.h
@@ -3,13 +3,13 @@
 
 #include "snake.h"
 
-class Controller {
- public:
+class Controller
+{
+public:
   void HandleInput(bool &running, Snake &snake) const;
 
- private:
-  void ChangeDirection(Snake &snake, Snake::Direction input,
-                       Snake::Direction opposite) const;
+private:
+  void ChangeDirection(Snake &snake, Snake::Direction input, Snake::Direction opposite) const;
 };
 
 #endif
